28d082b326cfd8835127f52cb8018efb40f98154
ame1252 <kyou611yu22@gmail.com>
2023-01-09 12:09:47 +0900

    wip

--- todolist/src/main/java/com/example/todolist/TodolistController.java
+++ todolist/src/main/java/com/example/todolist/TodolistController.java
@@ -2,6 +2,7 @@ package com.example.todolist;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -16,9 +17,15 @@ public class TodolistController {
 
     @GetMapping("/")
     public List<TodoItem> getAll() {
-        String query = "SELECT * from todo_item";
+        String query = "SELECT * FROM todo_item";
         TodoItemRowMapper rowMapper = new TodoItemRowMapper();
         return jdbcTemplate.query(query, rowMapper);
+    }
+
+    @PostMapping("/update")
+    public int update(){
+        String query = "UPDATE todo_item SET is_completed = true WHERE id = 2";
+        return jdbcTemplate.update(query);
     }
 
 }
