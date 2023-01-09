cc1229de905c8bb8a1e61fc525695b787f24818e
ame1252 <107744566+ame1252@users.noreply.github.com>
2023-01-09 12:42:27 +0900

    完了更新処理を実装した (#3)
    * wip
    
    * 完了更新処理を実装した

--- todolist/src/main/java/com/example/todolist/TodolistController.java
+++ todolist/src/main/java/com/example/todolist/TodolistController.java
@@ -2,6 +2,8 @@ package com.example.todolist;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -16,9 +18,15 @@ public class TodolistController {
 
     @GetMapping("/")
     public List<TodoItem> getAll() {
-        String query = "SELECT * from todo_item";
+        String query = "SELECT * FROM todo_item";
         TodoItemRowMapper rowMapper = new TodoItemRowMapper();
         return jdbcTemplate.query(query, rowMapper);
+    }
+
+    @PostMapping("/{id}/complete")
+    public int complete(@PathVariable long id) {
+        String query = "UPDATE todo_item SET is_completed = true WHERE id = ?";
+        return jdbcTemplate.update(query, id);
     }
 
 }
