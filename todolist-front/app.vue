99df7ad18503088ca788295526376093d1715966
agoetc <tkgw666private@gmail.com>
2023-01-22 15:30:56 +0900

    todoitemを表示するためのmock実装を追加

--- todolist-front/app.vue
+++ todolist-front/app.vue
@@ -1,5 +1,30 @@
+<script setup lang="ts">
+type TodoItem = {
+  id: number
+  title: string
+  body: string
+  is_completed: boolean
+}
+
+const value = (itemId: number): TodoItem =>{
+  return {id: itemId,
+    title: "たいとる",
+    body: "ぼでぃ",
+    is_completed: false
+  }
+} 
+const items = ref<TodoItem[]>([value(1), value(2), value(3)]);
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        <p>{{ item.id }}</p>
+        <p>{{ item.title }}</p>
+        <p>{{ item.body }}</p>
+        <p>{{ item.is_completed }}</p>
+      </li>
+    </ul>
   </div>
 </template>
