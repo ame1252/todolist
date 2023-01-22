d15738595c563627a6cffa5fd268415903ffbc24
agoetc <tkgw666private@gmail.com>
2023-01-22 16:58:03 +0900

    CORSの設定を追加

--- todolist/src/main/java/com/example/todolist/TodolistController.java
+++ todolist/src/main/java/com/example/todolist/TodolistController.java
@@ -1,16 +1,14 @@
 package com.example.todolist;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class TodolistController {
 
     @Autowired
